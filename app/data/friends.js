16380aff03558d8de248eef327e2fe5d9a651246
mtartan <mtartan@gmail.com>
2018-08-23 14:10:32 -0700

    first draft

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+var friends = [
+	
+	{
+		name: "Benedict Cumberbatch",
+		photo: "http://media.hollywood.com/images/638x425/1883655.jpg",
+		scores: [
+			"5",
+			"1",
+			"4",
+			"3",
+			"1",
+			"5",
+			"1",
+			"4",
+			"3",
+			"1"
+		]
+	},
+	{
+		name: "Emma Stone",
+		photo: "https://img2.thelist.com/img/gallery/the-stunning-transformation-of-emma-stone/her-original-fashion-inspiration.jpg",
+		scores: [
+			"3",
+			"1",
+			"5",
+			"4",
+			"1",
+			"5",
+			"1",
+			"1",
+			"5",
+			"3"
+		]
+	},
+	{
+		name: "Keira Knightley",
+		photo: "https://i.ytimg.com/vi/Agw5HKVD4Go/maxresdefault.jpg",
+		score: [
+			"3",
+			"1",
+			"4",
+			"1",
+			"4",
+			"3",
+			"5",
+			"1",
+			"5",
+			"4"		
+		]
+	}
+];
+
+module.exports = friends;
